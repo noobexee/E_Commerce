7fb97a8fcef3d4ac90823dd81cf41e3658210a50
Kornchawan Lorlohaphan <6733003221@student.chula.ac.th>
2025-05-16 11:46:00 +0700

    fix cartItem

--- cartItem.py
+++ cartItem.py
@@ -7,26 +7,27 @@ class CartItem:
             raise ValueError("Wrong input data type");
         if quantity <= 0:
             raise InvalidQuantityError("Initial quantity must be greater than 0.")
+            
         self._product = product
         self._quantity = quantity
 
-    def removeQuantity(self, quantity):
-        if(type(quantity) != int){
+    def removeQuantity(self, quantity: int):
+        if(type(quantity) != int):
             raise ValueError("Wrong input data type");
-        }
         if quantity <= 0:
             raise InvalidQuantityError("Quantity to remove must be positive.")
         if quantity > self._quantity:
             raise InsufficientQuantityError("Not enough quantity in cart to remove.")
+
         self._quantity -= quantity
         print(f"Successfully remove {quantity} {self._product.name} to CartItem")
 
     def addQuantity(self, quantity):
-        if(type(quantity) != int){
+        if(type(quantity) != int):
             raise ValueError("Wrong input data type");
-        }
         if quantity <= 0:
             raise InvalidQuantityError("Quantity to add must be positive.")
+
         self._quantity += quantity
         print(f"Successfully add {quantity} {self._product.name} to CartItem")
    
